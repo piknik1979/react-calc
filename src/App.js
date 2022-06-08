0dc01b962ab4705961f82f8f172870e566ef2909
piknik1979 <tpikniczka@gmail.com>
2022-06-08 19:10:52 +0100

    added styling to the grid

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
+import "./index.css";
 
 function App() {
   return (
-    <div className="calculator-grid">   {/* main calculator grid */}
-      <div className="output">           {/* // main output screen - whole black section and includes prev and curre op.   */}  
-        <div className="previous-operand"></div> 
-        <div className="current-operand"></div>  
+    <div className="calculator-grid">
+      {/* main calculator grid */}
+      <div className="output">
+        {" "}
+        {/* // main output screen - whole black section and includes prev and curre op.   */}
+        <div className="previous-operand">123.124</div>{" "}
+        {/* small numbers - previous calculation*/}
+        <div className="current-operand">123.124</div>{" "}
+        {/* large numbers - previous calculation*/}
       </div>
-      <button className="span-two">AC</button>  
-      <button className="DEL">DEL</button>  
-      <button className="all-clear">/</button>  
-      <button className="all-clear">1</button>  
-      <button className="all-clear">2</button>  
-      <button className="all-clear">3</button>  
-      <button className="all-clear">*</button>  
-      <button className="all-clear">4</button>  
-      <button className="all-clear">5</button>  
-      <button className="all-clear">6</button>  
-      <button className="all-clear">+</button>  
-      <button className="all-clear">7</button>  
-      <button className="all-clear">8</button>  
-      <button className="all-clear">9</button>  
-      <button className="all-clear">-</button>  
-      <button className="all-clear">.</button>  
-      <button className="all-clear">0</button>  
-      <button className="span-two">=</button>  
-   </div> 
+      <button className="span-two">AC</button>
+      <button className="DEL">DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
   );
-};
+}
 
 export default App;
+
+// // minuta 7.43
+// https://www.youtube.com/watch?v=DgRrrOt0Vr8&ab_channel=WebDevSimplified
