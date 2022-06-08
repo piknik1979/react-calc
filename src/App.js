e3a01d352c4dd904d8fe082a3d4b59396b4f1d82
piknik1979 <tpikniczka@gmail.com>
2022-06-08 18:31:44 +0100

    calculator grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="calculator-grid">   {/* main calculator grid */}
+      <div className="output">           {/* // main output screen - whole black section and includes prev and curre op.   */}  
+        <div className="previous-operand"></div> 
+        <div className="current-operand"></div>  
+      </div>
+      <button className="span-two">AC</button>  
+      <button className="DEL">DEL</button>  
+      <button className="all-clear">/</button>  
+      <button className="all-clear">1</button>  
+      <button className="all-clear">2</button>  
+      <button className="all-clear">3</button>  
+      <button className="all-clear">*</button>  
+      <button className="all-clear">4</button>  
+      <button className="all-clear">5</button>  
+      <button className="all-clear">6</button>  
+      <button className="all-clear">+</button>  
+      <button className="all-clear">7</button>  
+      <button className="all-clear">8</button>  
+      <button className="all-clear">9</button>  
+      <button className="all-clear">-</button>  
+      <button className="all-clear">.</button>  
+      <button className="all-clear">0</button>  
+      <button className="span-two">=</button>  
+   </div> 
   );
-}
+};
 
 export default App;
