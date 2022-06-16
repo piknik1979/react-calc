e93a9cb87d7bc6336f5929eb1f04a3253a1c964d
piknik1979 <tpikniczka@gmail.com>
2022-06-16 14:50:29 +0100

    replacing original Buttons with DigitButtons from the new component

--- src/App.js
+++ src/App.js
@@ -56,21 +56,23 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
+      {/* Setting Up Digits - replacing the original buttons with DigitButtons and '.' period button as well  */}
+      {/* Setting Up Operation Buttons, as well (/, +. -, *. */}
       <DigitButton digit="/" dispatch={dispatch} />
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
       <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
       <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
